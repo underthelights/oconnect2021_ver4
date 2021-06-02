3ed14784b86e2edfbee7b9b554848004a7dfe948
Aiden Kwak <duckracoon@gist.ac.kr>
2021-06-02 23:21:40 +0900

    first commit error fixed ver

---
+++ oconnect/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for oconnect project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oconnect.settings')
+
+application = get_asgi_application()
